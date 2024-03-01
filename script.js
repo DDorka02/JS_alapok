1a5cbcdf3e4ce599be5f4de6a093136d742724ad
DDorka02 <dobszaydorka@gmail.com>
2024-03-01 09:29:15 +0100

    lll

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ window.addEventListener("load", function () {
     elemekElerese2();
     elemekElerese3();
     elemekElerese4();
+    elemekFormazasa1();
+    esemenyvezerles1();
 });
 
 function elemekElerese1() {
@@ -10,22 +12,38 @@ function elemekElerese1() {
     console.log(ELEM)
 }
 function elemekElerese2() {
-    ELEM2=document.querySelector("#ide")
+    const ELEM2=document.querySelector("#ide")
     ELEM2.innerHTML="<p>Jó reggelt!</p>"
 }
 function elemekElerese3() {
     const ELEM3=document.querySelector(".ide")
     ELEM3.innerHTML="<p>Jó reggelt!</p>"
 }
-
 function elemekElerese4() {
     const ELEM4=document.querySelector(".lista")
-    let txt="<ul>"
+    let txt="<ul>";
     for (let index = 0; index < 5; index++) {
-        a = Math.floor(Math.random()*21+10)
-        txt+="<li>" + a +"</li>";  
+        a = Math.floor(Math.random()*21+10);
+        txt+=`<li> ${a} </li>`;  
     }
-    txt="</ul>"
-    ELEM4.innerHTML=txt
-    console.log(txt)
+    txt += "</ul>";
+    ELEM4.innerHTML=txt;
 }
+function elemekFormazasa1() {
+    const ELEM5=document.querySelector(".lista")
+    ELEM5.classList.add("formazott")
+}
+function esemenyvezerles1() {
+    const ELEM6=document.querySelector(".lista")
+    ELEM6.addEventListener("click", kattintasutan)
+    
+}
+function kattintas(){
+    const ELEM7=document.querySelector(".lista")
+    ELEM7=addEventListener("click", kattintasutan)
+    function kattintasutan(){
+       kattintas.document.querySelector(".lista")= ELEM7.innerHTML
+    }
+}
+f
+
